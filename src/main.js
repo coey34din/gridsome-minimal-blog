982f42a169a5d1864236ade04da1c460aa230763
coey34din <coey34din@gmail.com>
2019-11-04 12:25:03 +0800

    css

--- src/main.js
+++ src/main.js
@@ -17,7 +17,10 @@ export default function (Vue, { router, head, isClient }) {
   head.link.push({
     rel: 'stylesheet',
     href: 'https://fonts.googleapis.com/css?family=Stylish&display=swap'
-  }),
+    Vue.use(BootstrapVue)
+  }
+    
+    ),
 
 
   // Set default layout as a global component
