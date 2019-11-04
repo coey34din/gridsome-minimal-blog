54acb2ac1de83151182b9710d5af6c7a03f2f654
coey34din <coey34din@gmail.com>
2019-11-04 12:10:08 +0800

    css

--- src/main.js
+++ src/main.js
@@ -2,14 +2,21 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import DefaultLayout from '~/layouts/Default.vue'
-import DefaultLayout from '~/assets/styles.css'
-import DefaultLayout from '~/assets/boostrap.min.css'
 
 export default function (Vue, { router, head, isClient }) {
   head.link.push({
     rel: 'stylesheet',
     href: 'https://fonts.googleapis.com/css?family=Stylish&display=swap'
-  }),
+  },
+  {
+    rel: 'stylesheet',
+    href: 'assets/css/style.css'
+  },
+  {
+    rel: 'stylesheet',
+    href: 'assets/css/boostrap.min.css'
+  }
+  ),
 
 
   // Set default layout as a global component
